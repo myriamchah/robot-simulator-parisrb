7431efeae86ed15c704fc730f2ad26978f7c7fe3
Myriam Chahlafi <myriam.chah@gmail.com>
2019-09-11 20:41:14 +0200

    Create instructions for simulator

--- robot_simulator_test.rb
+++ robot_simulator_test.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
-require_relative 'robot_simulator'
+require_relative 'robot'
+require_relative 'simulator'
 
 class RobotTurningTest < Minitest::Test
   attr_reader :robot
@@ -116,17 +117,14 @@ class RobotSimulatorTest < Minitest::Test
   end
 
   def test_instructions_for_turning_left
-    skip
     assert_equal [:turn_left], simulator.instructions('L')
   end
 
   def test_instructions_for_turning_right
-    skip
     assert_equal [:turn_right], simulator.instructions('R')
   end
 
   def test_instructions_for_advancing
-    skip
     assert_equal [:advance], simulator.instructions('A')
   end
 
---
+++ simulator.rb
@@ -0,0 +1,14 @@
+class Simulator
+
+  def instructions(direction_path)
+    case direction_path
+    when 'L'
+      [:turn_left]
+    when 'R'
+      [:turn_right]
+    when 'A'
+      [:advance]
+    end
+  end
+end
+
