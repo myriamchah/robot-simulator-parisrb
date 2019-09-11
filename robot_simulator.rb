542acd44e8f765c1754b2ee2db6e5da2258ae63f
Myriam Chahlafi <myriam.chah@gmail.com>
2019-09-11 20:12:15 +0200

    Create rotation r/l methods

--- robot_simulator.rb
+++ robot_simulator.rb
@@ -6,4 +6,30 @@ attr_reader :bearing
     raise ArgumentError, "Not a possible direction, please use #{possible_directions}" unless possible_directions.include? direction
     @bearing = direction
   end
+
+  def turn_right
+    case bearing
+    when :north
+      orient(:east)
+    when :east
+      orient(:south)
+    when :south
+      orient(:west)
+    when :west
+      orient(:north)
+    end
+  end
+
+  def turn_left
+    case bearing
+    when :north
+      orient(:west)
+    when :east
+      orient(:north)
+    when :south
+      orient(:east)
+    when :west
+      orient(:south)
+    end
+  end
 end
