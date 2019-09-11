f8f16a8e4762e538281da7b8dd98dd9ab782dbe8
Myriam Chahlafi <myriam.chah@gmail.com>
2019-09-11 21:08:37 +0200

    Create place and evalute simulator methods

--- robot_simulator_test.rb
+++ robot_simulator_test.rb
@@ -134,7 +134,6 @@ class RobotSimulatorTest < Minitest::Test
   end
 
   def test_instruct_robot
-    skip
     robot = Robot.new
     simulator.place(robot, x: -2, y: 1, direction: :east)
     simulator.evaluate(robot, 'RLAALAL')
--- simulator.rb
+++ simulator.rb
@@ -12,5 +12,22 @@ class Simulator
       end
     end
   end
+
+  def evaluate(robot, path)
+    instructions(path).each do |dir|
+      robot.send(dir)
+    end
+  end
+
+  def place(robot, x: , y: , direction: )
+    robot.at(x,y)
+    robot.orient(direction)
+  end
 end
 
+
+
+ # simulator.place(robot, x: -2, y: 1, direction: :east)
+ #    simulator.evaluate(robot, 'RLAALAL')
+ #    assert_equal [0, 2], robot.coordinates
+ #    assert_equal :west, robot.bearing
